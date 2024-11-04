19e7e4cf875b7ed48e5a3eccad36777f4463400e
Rajesh Zurange <zurangerajesh@gmail.com>
2024-11-04 15:39:53 +0530

    Create Aggregate_Functions.sql

---
+++ Aggregate_Functions.sql
@@ -0,0 +1,50 @@
+--- Question 1: Not Boring Movies
+--- https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+--- Question 2: Average Selling Price 
+--- https://leetcode.com/problems/average-selling-price/?envType=study-plan-v2&envId=top-sql-50
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+--- Question 3: Project Employee 1 
+--- https://leetcode.com/problems/project-employees-i/?envType=study-plan-v2&envId=top-sql-50
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+--- Question 4: Percentage of User Attended Contest 
+
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+--- Question 5: Queries, Quality and Percentage 
+--- https://leetcode.com/problems/queries-quality-and-percentage/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+--- Question 6: Monthly Transaction 1 
+--- https://leetcode.com/problems/monthly-transactions-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+--- Question 7: Immediate Food delivery
+--- https://leetcode.com/problems/immediate-food-delivery-ii/?envType=study-plan-v2&envId=top-sql-50
+
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+--- Question 8: Game Play Analysis 
+--- https://leetcode.com/problems/game-play-analysis-iv/description/?envType=study-plan-v2&envId=top-sql-50
+
